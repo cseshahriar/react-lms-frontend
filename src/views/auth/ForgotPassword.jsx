a8185f08aa135755a6bec475421350ac209f8bf2
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-05-12 08:59:12 +0600

    feat: password forgot

--- src/views/auth/ForgotPassword.jsx
+++ src/views/auth/ForgotPassword.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import BaseHeader from '../partials/BaseHeader'
 import BaseFooter from '../partials/BaseFooter'
-import { Link } from 'react-router-dom'
-
+import apiInstance from '../../utils/axios'
+import { Link, useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
 
 function ForgotPassword() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      console.log(email);
+      await apiInstance.get(`user/password-reset/${email}/`).then(res => {
+        console.log(res.data)
+        setIsLoading(false)
+        Swal.fire({
+          title: "Password Reset Email Sent Successful",
+          icon: "success",
+          draggable: true
+        });
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
       <BaseHeader />
@@ -20,24 +44,37 @@ function ForgotPassword() {
                     Let's help you get back into your account
                   </span>
                 </div>
-                <form className="needs-validation" noValidate="">
+                <form className="needs-validation" noValidate="" onSubmit={handleSubmit}>
                   <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email Address</label>
                     <input
                       type="email"
                       id="email"
-                      className="form-control"
                       name="email"
-                      placeholder="johndoe@gmail.com"
                       required=""
+                      className="form-control"
+                      placeholder="johndoe@gmail.com"
+                      onChange={(e) => setEmail(e.target.value)}
                     />
                   </div>
 
                   <div>
                     <div className="d-grid">
-                      <button type="submit" className="btn btn-primary">
-                        Reset Password <i className='fas fa-arrow-right'></i>
-                      </button>
+                      {
+                        isLoading === true && (
+                          <button type="submit" className="btn btn-primary" disabled>
+                            Processing <i className='fas fa-spinner fa-spin'></i>
+                          </button>
+                        )
+                      }
+
+                      {
+                        isLoading === false && (
+                          <button type="submit" className="btn btn-primary">
+                            Reset Password <i className='fas fa-arrow-right'></i>
+                          </button>
+                        )
+                      }
                     </div>
                   </div>
                 </form>
