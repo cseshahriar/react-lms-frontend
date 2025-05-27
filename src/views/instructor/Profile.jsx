893853fa77d1f979e4d64b7627c2e2df8a58a39f
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-05-27 08:40:56 +0600

    instructure profile

--- src/views/instructor/Profile.jsx
+++ src/views/instructor/Profile.jsx
@@ -1,10 +1,88 @@
-import React from 'react'
-import BaseHeader from '../partials/BaseHeader'
-import BaseFooter from '../partials/BaseFooter'
-import Sidebar from './Partials/Sidebar'
-import Header from './Partials/Header'
+import React, { useState, useEffect, useContext } from "react";
+import BaseHeader from "../partials/BaseHeader";
+import BaseFooter from "../partials/BaseFooter";
+import Sidebar from "./Partials/Sidebar";
+import Header from "./Partials/Header";
+
+import useAxios from "../../utils/useAxios";
+import UserData from "../plugin/UserData";
+import Toast from "../plugin/Toast";
+import { ProfileContext } from "../plugin/Context";
 
 function Profile() {
+    const [profile, setProfile] = useContext(ProfileContext);
+    const [profileData, setProfileData] = useState({
+        image: "",
+        full_name: "",
+        about: "",
+        country: "",
+    });
+    const [imagePreview, setImagePreview] = useState("");
+
+    const fetchProfile = () => {
+        useAxios.get(`user/profile/${UserData()?.user_id}/`).then((res) => {
+            console.log(res.data);
+            setProfile(res.data);
+            setProfileData(res.data);
+            setImagePreview(res.data.image);
+        });
+    };
+
+    useEffect(() => {
+        fetchProfile();
+    }, []);
+
+    const handleProfileChange = (event) => {
+        setProfileData({
+            ...profileData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleFileChange = (event) => {
+        const selectedFile = event.target.files[0];
+        setProfileData({
+            ...profileData,
+            [event.target.name]: selectedFile,
+        });
+
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            setImagePreview(reader.result);
+        };
+
+        if (selectedFile) {
+            reader.readAsDataURL(selectedFile);
+        }
+    };
+
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+
+        const res = await useAxios.get(`user/profile/${UserData()?.user_id}/`);
+        const formdata = new FormData();
+        if (profileData.image && profileData.image !== res.data.image) {
+            formdata.append("image", profileData.image);
+        }
+
+        formdata.append("full_name", profileData.full_name);
+        formdata.append("about", profileData.about);
+        formdata.append("country", profileData.country);
+
+        await useAxios
+            .patch(`user/profile/${UserData()?.user_id}/`, formdata, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            })
+            .then((res) => {
+                console.log(res.data);
+                setProfile(res.data);
+            });
+    };
+
+    console.log(imagePreview);
+
     return (
         <>
             <BaseHeader />
@@ -22,27 +100,28 @@ function Profile() {
                                 {/* Card header */}
                                 <div className="card-header">
                                     <h3 className="mb-0">Profile Details</h3>
-                                    <p className="mb-0">
-                                        You have full control to manage your own account setting.
-                                    </p>
+                                    <p className="mb-0">You have full control to manage your own account setting.</p>
                                 </div>
                                 {/* Card body */}
-                                <form className="card-body">
+                                <form className="card-body" onSubmit={handleFormSubmit}>
                                     <div className="d-lg-flex align-items-center justify-content-between">
                                         <div className="d-flex align-items-center mb-4 mb-lg-0">
                                             <img
-                                                src="https://eduport.webestica.com/assets/images/avatar/09.jpg"
+                                                src={imagePreview}
                                                 id="img-uploaded"
                                                 className="avatar-xl rounded-circle"
                                                 alt="avatar"
-                                                style={{ width: "100px", height: "100px", borderRadius: "50%", objectFit: "cover" }}
+                                                style={{
+                                                    width: "100px",
+                                                    height: "100px",
+                                                    borderRadius: "50%",
+                                                    objectFit: "cover",
+                                                }}
                                             />
                                             <div className="ms-3">
                                                 <h4 className="mb-0">Your avatar</h4>
-                                                <p className="mb-0">
-                                                    PNG or JPG no bigger than 800px wide and tall.
-                                                </p>
-                                                <input type="file" className='form-control mt-3' name="" id="" />
+                                                <p className="mb-0">PNG or JPG no bigger than 800px wide and tall.</p>
+                                                <input type="file" className="form-control mt-3" name="image" onChange={handleFileChange} id="" />
                                             </div>
                                         </div>
                                     </div>
@@ -57,13 +136,7 @@ function Profile() {
                                                 <label className="form-label" htmlFor="fname">
                                                     Full Name
                                                 </label>
-                                                <input
-                                                    type="text"
-                                                    id="fname"
-                                                    className="form-control"
-                                                    placeholder="First Name"
-                                                    required=""
-                                                />
+                                                <input type="text" id="fname" className="form-control" placeholder="First Name" required="" value={profileData.full_name} onChange={handleProfileChange} name="full_name" />
                                                 <div className="invalid-feedback">Please enter first name.</div>
                                             </div>
                                             {/* Last name */}
@@ -71,7 +144,7 @@ function Profile() {
                                                 <label className="form-label" htmlFor="lname">
                                                     About Me
                                                 </label>
-                                                <textarea name="" id="" cols="30" rows="5" className='form-control'></textarea>
+                                                <textarea onChange={handleProfileChange} name="about" id="" cols="30" rows="5" className="form-control" value={profileData.about}></textarea>
                                                 <div className="invalid-feedback">Please enter last name.</div>
                                             </div>
 
@@ -80,19 +153,13 @@ function Profile() {
                                                 <label className="form-label" htmlFor="editCountry">
                                                     Country
                                                 </label>
-                                                <input
-                                                    type="text"
-                                                    id="country"
-                                                    className="form-control"
-                                                    placeholder="Country"
-                                                    required=""
-                                                />
+                                                <input type="text" id="country" className="form-control" placeholder="Country" required="" value={profileData.country} onChange={handleProfileChange} name="country" />
                                                 <div className="invalid-feedback">Please choose country.</div>
                                             </div>
                                             <div className="col-12">
                                                 {/* Button */}
                                                 <button className="btn btn-primary" type="submit">
-                                                    Update Profile <i className='fas fa-check-circle'></i>
+                                                    Update Profile <i className="fas fa-check-circle"></i>
                                                 </button>
                                             </div>
                                         </div>
@@ -100,14 +167,13 @@ function Profile() {
                                 </form>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </section>
 
             <BaseFooter />
         </>
-    )
+    );
 }
 
-export default Profile
+export default Profile;
