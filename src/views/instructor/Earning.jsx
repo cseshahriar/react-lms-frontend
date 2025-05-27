8b480965f786279e3e0dbf6a59571f0faac024e1
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-05-27 08:56:34 +0600

    teacher earning component

--- src/views/instructor/Earning.jsx
+++ src/views/instructor/Earning.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
-import Sidebar from './Partials/Sidebar'
-import Header from './Partials/Header'
+import { useState, useEffect } from "react";
+import moment from "moment";
 
-import BaseHeader from '../partials/BaseHeader'
-import BaseFooter from '../partials/BaseFooter'
+import Sidebar from "./Partials/Sidebar";
+import Header from "./Partials/Header";
+import BaseHeader from "../partials/BaseHeader";
+import BaseFooter from "../partials/BaseFooter";
+
+import useAxios from "../../utils/useAxios";
+import Useta from "../plugin/UserData";
+import { teacherId } from "../../utils/constants";
+import UserData from "../plugin/UserData";
 
 function Earning() {
+    const [stats, setStats] = useState([]);
+    const [earning, setEarning] = useState([]);
+    const [bestSellingCourse, setBestSellingCourse] = useState([]);
+
+    useEffect(() => {
+        useAxios.get(`teacher/summary/${UserData()?.teacher_id}/`).then((res) => {
+            console.log('teacher summery: ', res.data[0]);
+            setStats(res.data[0]);
+        });
+
+        useAxios.get(`teacher/all-months-earning/${UserData()?.teacher_id}/`).then((res) => {
+            console.log('monthly earnings ', res.data);
+            setEarning(res.data);
+        });
+
+        useAxios.get(`teacher/best-course-earning/${UserData()?.teacher_id}/`).then((res) => {
+            console.log('best course earning ', res.data);
+            setBestSellingCourse(res.data);
+        });
+    }, []);
     return (
         <>
             <BaseHeader />
@@ -21,29 +47,17 @@ function Earning() {
                             <div className="card mb-4">
                                 <div className="card-body">
                                     <h3 className="mb-0">Earnings</h3>
-                                    <p className="mb-0">
-                                        You have full control to manage your own account setting.
-                                    </p>
+                                    <p className="mb-0">You have full control to manage your own account setting.</p>
                                 </div>
                             </div>
                             <div className="card mb-4">
                                 <div className="card-header d-flex justify-content-between align-items-center">
                                     <h4 className="mb-0">Earnings</h4>
                                     <div className="dropdown dropstart">
-                                        <a
-                                            className="btn-icon btn btn-ghost btn-sm rounded-circle"
-                                            href="#"
-                                            role="button"
-                                            id="paymentDropdown"
-                                            data-bs-toggle="dropdown"
-                                            aria-expanded="false"
-                                        >
+                                        <a className="btn-icon btn btn-ghost btn-sm rounded-circle" href="#" role="button" id="paymentDropdown" data-bs-toggle="dropdown" aria-expanded="false">
                                             <i className="fe fe-more-vertical" />
                                         </a>
-                                        <div
-                                            className="dropdown-menu dropdown-menu-end"
-                                            aria-labelledby="paymentDropdown"
-                                        >
+                                        <div className="dropdown-menu dropdown-menu-end" aria-labelledby="paymentDropdown">
                                             <span className="dropdown-header">Setting</span>
                                             <a className="dropdown-item" href="#">
                                                 30 Days
@@ -55,26 +69,22 @@ function Earning() {
                                     </div>
                                 </div>
                                 <div className="card-body">
-                                  <div className="row">
-                                    <div className="col-xl-6 col-lg-6 col-md-12 col-12 mb-3 mb-lg-0">
-                                      <div className="border p-3 rounded shadow-sm">
-                                        <i className="fe fe-shopping-cart icon-shape icon-sm rounded-3 bg-light-success text-dark-success mt-2" />
-                                        <h3 className="display-4 fw-bold mt-3 mb-0">
-                                          ${"95.00"}
-                                        </h3>
-                                        <span>Monthly Earnings (Jan)</span>
-                                      </div>
+                                    <div className="row">
+                                        <div className="col-xl-6 col-lg-6 col-md-12 col-12 mb-3 mb-lg-0">
+                                            <div className="border p-3 rounded shadow-sm">
+                                                <i className="fe fe-shopping-cart icon-shape icon-sm rounded-3 bg-light-success text-dark-success mt-2" />
+                                                <h3 className="display-4 fw-bold mt-3 mb-0">${stats.total_revenue?.toFixed(2)}</h3>
+                                                <span>Monthly Earnings (Jan)</span>
+                                            </div>
+                                        </div>
+                                        <div className="col-xl-6 col-lg-6 col-md-12 col-12 mb-3 mb-lg-0">
+                                            <div className="border p-3 rounded shadow-sm">
+                                                <i className="fe fe-shopping-cart icon-shape icon-sm rounded-3 bg-light-success text-dark-success mt-2" />
+                                                <h3 className="display-4 fw-bold mt-3 mb-0">${stats.monthly_revenue?.toFixed(2)}</h3>
+                                                <span>Your Revenue</span>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div className="col-xl-6 col-lg-6 col-md-12 col-12 mb-3 mb-lg-0">
-                                      <div className="border p-3 rounded shadow-sm">
-                                        <i className="fe fe-shopping-cart icon-shape icon-sm rounded-3 bg-light-success text-dark-success mt-2" />
-                                        <h3 className="display-4 fw-bold mt-3 mb-0">
-                                          ${"25.00"}
-                                        </h3>
-                                        <span>Your Revenue</span>
-                                      </div>
-                                    </div>
-                                  </div>
                                 </div>
                             </div>
                             {/* Card */}
@@ -94,55 +104,48 @@ function Earning() {
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <td>
-                                                    <a href="#" className='text-decoration-none text-dark'>
-                                                        <div className="d-flex align-items-center">
-                                                            <img
-                                                                src="https://geeksui.codescandy.com/geeks/assets/images/course/course-laravel.jpg"
-                                                                alt="course"
-                                                                style={{ width: "100px", height: "70px", borderRadius: "50%", objectFit: "cover" }}
-                                                                className="rounded img-4by3-lg"
-                                                            />
-                                                            <h5 className="mb-0 ms-3 ">
-                                                                Building Scalable APIs with GraphQL
-                                                            </h5>
-                                                        </div>
-                                                    </a>
-                                                </td>
-                                                <td>34</td>
-                                                <td>$3,145.23</td>
-                                                <td className="align-middle border-top-0">
-                                                    <span className="dropdown dropstart">
-                                                        <a
-                                                            className="btn-icon btn btn-ghost btn-sm rounded-circle"
-                                                            href="#"
-                                                            role="button"
-                                                            id="courseDropdown1"
-                                                            data-bs-toggle="dropdown"
-                                                            data-bs-offset="-20,20"
-                                                            aria-expanded="false"
-                                                        >
-                                                            <i className="fe fe-more-vertical" />
+                                            {bestSellingCourse?.map((bestCourse, index) => (
+                                                <tr key={index}>
+                                                    <td>
+                                                        <a href="#" className="text-decoration-none text-dark">
+                                                            <div className="d-flex align-items-center">
+                                                                <img
+                                                                    src={"http://127.0.0.1:8000" + bestCourse.course_image}
+                                                                    alt={bestCourse.course_title}
+                                                                    style={{
+                                                                        width: "100px",
+                                                                        height: "70px",
+                                                                        borderRadius: "50%",
+                                                                        objectFit: "cover",
+                                                                    }}
+                                                                    className="rounded img-4by3-lg"
+                                                                />
+                                                                <h5 className="mb-0 ms-3 ">{bestCourse.course_title}</h5>
+                                                            </div>
                                                         </a>
-                                                        <span
-                                                            className="dropdown-menu"
-                                                            aria-labelledby="courseDropdown1"
-                                                        >
-                                                            <span className="dropdown-header">Setting</span>
-                                                            <a className="dropdown-item" href="#">
-                                                                <i className="fe fe-edit dropdown-item-icon" />
-                                                                Edit
+                                                    </td>
+                                                    <td>{bestCourse.sales}</td>
+                                                    <td>${bestCourse.revenue}</td>
+                                                    <td className="align-middle border-top-0">
+                                                        <span className="dropdown dropstart">
+                                                            <a className="btn-icon btn btn-ghost btn-sm rounded-circle" href="#" role="button" id="courseDropdown1" data-bs-toggle="dropdown" data-bs-offset="-20,20" aria-expanded="false">
+                                                                <i className="fe fe-more-vertical" />
                                                             </a>
-                                                            <a className="dropdown-item" href="#">
-                                                                <i className="fe fe-trash dropdown-item-icon" />
-                                                                Remove
-                                                            </a>
+                                                            <span className="dropdown-menu" aria-labelledby="courseDropdown1">
+                                                                <span className="dropdown-header">Setting</span>
+                                                                <a className="dropdown-item" href="#">
+                                                                    <i className="fe fe-edit dropdown-item-icon" />
+                                                                    Edit
+                                                                </a>
+                                                                <a className="dropdown-item" href="#">
+                                                                    <i className="fe fe-trash dropdown-item-icon" />
+                                                                    Remove
+                                                                </a>
+                                                            </span>
                                                         </span>
-                                                    </span>
-                                                </td>
-                                            </tr>
-
+                                                    </td>
+                                                </tr>
+                                            ))}
                                         </tbody>
                                     </table>
                                 </div>
@@ -158,17 +161,30 @@ function Earning() {
                                     <table className="table mb-0">
                                         <thead className="table-light">
                                             <tr>
-
                                                 <th>Month</th>
                                                 <th>Amount</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <td>January 2024</td>
-                                                <td>$1200</td>
-                                            </tr>
-
+                                            {earning?.map((e, index) => (
+                                                <tr key={index}>
+                                                    <td>
+                                                        {e.month === 1 && "January"}
+                                                        {e.month === 2 && "February"}
+                                                        {e.month === 3 && "March"}
+                                                        {e.month === 4 && "April"}
+                                                        {e.month === 5 && "May"}
+                                                        {e.month === 6 && "June"}
+                                                        {e.month === 7 && "July"}
+                                                        {e.month === 8 && "August"}
+                                                        {e.month === 9 && "Sepetember"}
+                                                        {e.month === 10 && "OCtober"}
+                                                        {e.month === 11 && "November"}
+                                                        {e.month === 12 && "December"}
+                                                    </td>
+                                                    <td>${e.total_earning?.toFixed(2)}</td>
+                                                </tr>
+                                            ))}
                                         </tbody>
                                     </table>
                                 </div>
@@ -180,7 +196,7 @@ function Earning() {
 
             <BaseFooter />
         </>
-    )
+    );
 }
 
-export default Earning
+export default Earning;
